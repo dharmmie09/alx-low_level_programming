a97d1f49140173616d0fed9d2efb9c366b68be32
dharmmie09 <dharmmiedaniel@gmail.com>
2023-08-11 19:31:04 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the beginning
+ *
+ * Description: this  frst c code
+ * Return: Always (0)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
