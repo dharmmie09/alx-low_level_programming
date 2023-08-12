8362774ec2eed01fdb465c65986710ff0e34b5c9
dharmmie09 <dharmmiedaniel@gmail.com>
2023-08-11 20:01:47 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: This string as indicated
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("wish proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
