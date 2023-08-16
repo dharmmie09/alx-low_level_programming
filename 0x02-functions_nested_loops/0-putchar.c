ad83098e98865e05166731fdf47157b6c26433d9
dharmmie09 <dharmmiedaniel@gmail.com>
2023-08-16 12:51:32 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  main - short start
+ *  _putchar: This is the main program
+ *  Description: This is the program itself
+ *  Return: Depends on instructions
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
