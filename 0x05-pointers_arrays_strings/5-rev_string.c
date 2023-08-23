e6364775227e7c52683807fc2afba7f8af5e93db
dharmmie09 <dharmmiedaniel@gmail.com>
2023-08-22 18:16:28 -0700

    revstring

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+include "main.h"
+#include <string.h>
+/**
+ * rev_string -reverse function
+ * @s: is the string used by user
+ * */
+
+void rev_string(char *s)
+{
+	int n = strlen(s);
+	int j = 0;
+
+	while (j < n / 2)
+	{
+		char rev = s[j];
+
+		s[j] = s[n - j - 1];
+		s[n - j - 1] = rev;
+		j++;
+	}
+}
