f2bacdedddc52a351b4373abef5fe9dd1af1fedd
dharmmie09 <dharmmiedaniel@gmail.com>
2023-08-22 17:47:08 -0700

    printrev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - is the function
+ * @s: is the pointe
+ */
+
+void print_rev(char *s)
+{
+	char *ptr = s + strlen(s) - 1;
+
+	while (ptr >= s)
+	{
+		putchar(*ptr);
+		ptr--;
+	}
+
+	putchar ('/n');
+}
