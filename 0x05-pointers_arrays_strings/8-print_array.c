7d040bd65cee272083d520efa11cf8050a11b82c
dharmmie09 <dharmmiedaniel@gmail.com>
2023-08-22 19:10:31 -0700

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - function to print array
+ * @n:the second integer
+ * @a: is the array
+ */
+
+void print_array(int *a, int n)
+{
+
+	int index_f_array;
+
+	for (index_of_array = 0; index_of_array < n; index_of_array++)
+	{
+
+		printf("%d", a[index_of_array]);
+		if (index_of_array != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
