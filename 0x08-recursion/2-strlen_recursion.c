a885625ccd9d44aa300cd70387a6f91d607c9eec
dharmmie09 <dharmmiedaniel@gmail.com>
2023-08-30 14:04:07 -0700

    fatorial

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen_recursion - is the string length
+ * @s: is the string pointer
+ * Return: as specified
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
