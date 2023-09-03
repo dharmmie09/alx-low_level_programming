b40310e15673e19ad6420c408a6e3ab525236a39
dharmmie09 <dharmmiedaniel@gmail.com>
2023-09-03 14:17:05 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -is the name of the function
+ * @argc: is the argument to main
+ * @argv: is the argument array
+ * Return: as 0 always
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf(''%s\n", argv[0]);
+	}
+	return (0)
+}
