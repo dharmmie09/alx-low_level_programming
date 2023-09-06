ed05194d7eb6f645dc5c62d282241fa849c24162
dharmmie09 <dharmmiedaniel@gmail.com>
2023-09-06 15:31:30 -0700

    freegrid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - is the dimension function
+ * @grid: is the width of the array
+ * @height: is the array
+ * Return: as specified
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+	{
+		return;
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
