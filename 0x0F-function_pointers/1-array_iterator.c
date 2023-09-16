5c1f505c3831be79993aa90043d0db26edb586cc
dharmmie09 <dharmmiedaniel@gmail.com>
2023-09-16 14:31:21 -0700

    printname

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - is function used to print element in an array
+ * @array: contains all the element given
+ * @size: is the size of the array element
+ * @action: is the pointer of the function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
