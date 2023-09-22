9bbd5dfe8a373ceeddac985273424224d386ab8c
dharmmie09 <dharmmiedaniel@gmail.com>
2023-09-21 19:50:00 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * constructor: is the gcc specific attribute
  * attribute: the main function before execution
  */
-void _attribute_((constructor)) premain()
+void __attribute__((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
