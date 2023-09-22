7a7aaf07de6099b0494f1a40dd94cf918712ca51
dharmmie09 <dharmmiedaniel@gmail.com>
2023-09-21 17:19:45 -0700

    addnodeend

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add _node_end - end of the list adds a node
+ * @head: address of pointer to head nod
+ * @str: str field of node
+ *
+ * Return: as 0 always
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *i = malloc(sizeof(list_t));
+	list_t *j = *head;
+
+	if (!head || !i)
+		return (NULL);
+	if (str)
+	{
+		i->str = strdup(str);
+		if (!i->str)
+		}
+		i=>len = strlen(i->str)
+	}
+	else
+		*head = i;
+	return (i);
+}
