c6af8baba2086198c45983e1fe0dadb1f5dbd765
dharmmie09 <dharmmiedaniel@gmail.com>
2023-09-21 19:46:34 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+#include "lists.h"
 /**
- * bmain - attribute to  executed before main
- * attribute - is the character
+ * premain - name of the function
+ * constructor: is the gcc specific attribute
+ * attribute: the main function before execution
  */
-
-void __attribute__ ((constructor)) bmain()
+void _attribute_((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
