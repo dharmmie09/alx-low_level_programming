c30c063fc5ad9ccf82009bb7294ef152ef361a2b
dharmmie09 <dharmmiedaniel@gmail.com>
2023-09-21 17:46:51 -0700

    freelist

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *  free_list -list of all free nodes
+ *  @head: pointer to head node
+ *
+ *  Return: as 0
+ */
+void free_list(list_t *head_)
+{
+	list_t *j, *i;
+	
+	if (!head)
+		return;
+
+	j = head;
+	while (j)
+	{
+		i = j->next;
+		free(j->str);
+		free(j);
+		j = i;
+	}
+}
