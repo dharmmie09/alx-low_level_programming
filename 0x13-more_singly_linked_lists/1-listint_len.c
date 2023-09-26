3244cc648ed4997f513a9800e9dfa0fa4b6cb5dc
dharmmie09 <dharmmiedaniel@gmail.com>
2023-09-26 15:15:08 -0700

    listint

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,7 +8,7 @@
 size_t listint_len(const listint_t *h)
 {
 	const listint_t *j;
-	size_t i -0;
+	size_t i = 0;
 	
 
 	j = h;
@@ -16,7 +16,7 @@ size_t listint_len(const listint_t *h)
 
 	while (j != NULL)
 	{
-		j = j->next
+		j = j->next;
 		i++;
 	}
 	return (i);
