78c1b11fa6a41f7f8f810d8dc984751ab942b0b0
dharmmie09 <dharmmiedaniel@gmail.com>
2023-09-26 15:10:12 -0700

    listint

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len - number of the element in a list are return
+ * @h: pointer to the list
+ * Return: as 0
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *j;
+	size_t i -0;
+	
+
+	j = h;
+
+
+	while (j != NULL)
+	{
+		j = j->next
+		i++;
+	}
+	return (i);
+}
