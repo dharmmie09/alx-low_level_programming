1d7896d320ace5bfbf6782c57dd20f524a389ca9
dharmmie09 <dharmmiedaniel@gmail.com>
2023-09-27 02:09:27 -0700

    insertnodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,12 +24,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 
-		j_ > next = *head;
+		j-> next = *head;
 		*head = j;
 		return (j);
 	}
 	current = *head;
-	for (i = 0; i < idx - 1; i++
+	for (i = 0; i < idx - 1; i++)
 	{
 		if (current == NULL)
 
